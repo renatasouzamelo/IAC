206d79f8d1edafb25af4f8bbc59df3e0e64310d2
renata <renata.melo89@outlook.com>
2023-01-20 23:31:17 -0300

    alter nohup

--- create_ec2_aws/main.tf
+++ create_ec2_aws/main.tf
@@ -18,7 +18,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-095413544ce52437d"
   instance_type = "t2.micro"
   key_name = "iac_tf"
+  user_data = <<-EOF
+                #!/bin/bash
+                cd /home/ubuntu
+                echo “<h1>Mensagem a ser mostrada</h1>” > index.js
+                nohup busybox httpd -f -p 8080 &
+                EOF
   tags = {
-    Name = "InstanceOne"   #NAME INSTANCE
+    Name = "InstanceTwo"   #NAME INSTANCE
   }
 }
