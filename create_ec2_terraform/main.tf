e3c6711fd4ec6b0d68dcf3012a6cf76b8d1b54d7
renata <renata.melo89@outlook.com>
2023-01-26 18:37:51 -0300

    subindo EC2 na AWS usando Ansible

---
+++ create_ec2_terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.3.7"
+}
+
+provider "aws" {
+  profile = "default"  
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {  
+  ami           = "ami-095413544ce52437d"
+  instance_type = "t2.micro"
+  key_name = "iac_tf"
+  # */user_data = <<-EOF
+#           #!/bin/bash
+#           cd /home/ubuntu
+#           echo “<h1>Mensagem a ser mostrada</h1>” > index.js
+#           nohup busybox httpd -f -p 8080 &
+#           EOF
+  tags = {
+    Name = "InstanceTwo"   #NAME INSTANCE
+  }
+}
